791959715717626746c2fd2cd687adab6a2fa38d
mdanikibun <md.anikibun@gmail.com>
2026-08-06 14:07:58 +0700

    feat(fence): spawn fence + detect lane

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Chunk : MonoBehaviour
+{
+    [SerializeField] GameObject fencePrefab;
+    [SerializeField] float[] lanes = {-3.5f, 0f, 3.5f};
+
+    void Start() {
+        SpawnFence();
+    }
+
+    void SpawnFence() {
+        List<int> availableLanes = new List<int>() {0, 1, 2};
+        int fencesToSpawn = Random.Range(0, lanes.Length);
+
+        for (int i = 0; i < fencesToSpawn; i++) {
+
+            if (availableLanes.Count <= 0) break;
+
+            int randomLaneIndex = Random.Range(0, availableLanes.Count);
+            int selectedLane = availableLanes[randomLaneIndex];
+            availableLanes.RemoveAt(randomLaneIndex);
+
+            Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
+            Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+        }
+    }
+}
